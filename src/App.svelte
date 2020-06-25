de801bce381477bc54aea14d86bc03673dcd2d75
lomeat <lom3at@gmail.com>
2020-06-25 14:52:31 +0300

    init project

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let name;
+</script>
+
+<h1>Hello, {name}</h1>
+
+<style lang="scss">
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(*) {
+    margin: 0;
+    padding: 0;
+    user-select: none;
+    outline: none;
+  }
+
+  h1 {
+    color: $color;
+  }
+</style>
